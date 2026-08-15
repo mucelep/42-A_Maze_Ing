7316688daf018ff6e631f760c2984dc7b6d9a023
Muhammed Ömer CELEP <celepm82@gmail.com>
2026-08-15 00:17:40 +0000

    remove_vall and generate_maze

--- mazegen/maze_generator.py
+++ mazegen/maze_generator.py
@@ -36,8 +36,8 @@ class MazeGenerator:
 
     def _generate_maze(self):
         start_x, start_y = 0, 0
-        visited: set[tuple[int, int]] = set()
-        stack:   list[tuple[int, int]]
+        visited: set[tuple[int, int]] = set()#gezilen hücrelerelerin kordinatları
+        stack:   list[tuple[int, int]]#islem yaptıgımız hücrelerin yolu
         
         visited.add((start_x, start_y))
         stack = [(start_x, start_y)]
@@ -49,7 +49,11 @@ class MazeGenerator:
             if neighbors:
                 new_x, new_y, direction = random.choice(neighbors)
                 self._remove_wall(x, y, new_x, new_y, direction)
-                    
+
+                visited.add((new_x, new_y))
+                stack.append((new_x, new_y))
+            else:
+                stack.pop()
 
 
 
@@ -74,4 +78,18 @@ class MazeGenerator:
         return neighbors
 
     def _remove_wall(self, x: int, y: int, new_x: int, new_y, direction: str) -> None:
-        pass
+        current_cell: Cell = self.grid[y][x]
+        neighbor_cell: Cell = self.grid[new_y][new_x]
+
+        if direction == "N":
+            current_cell.north = False
+            neighbor_cell.south = False
+        elif direction == "S":
+            current_cell.south = False
+            neighbor_cell.north = False
+        elif direction == "E":
+            current_cell.east = False
+            neighbor_cell.west = False
+        elif direction == "W":
+            current_cell.west = False
+            neighbor_cell.east = False
