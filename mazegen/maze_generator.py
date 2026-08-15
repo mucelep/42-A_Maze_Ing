2a1ba70eb1a3ba33d2586380fbe79dc836c26d9c
Muhammed Ömer CELEP <celepm82@gmail.com>
2026-08-15 00:02:46 +0000

    unvisited_neigbors

--- mazegen/maze_generator.py
+++ mazegen/maze_generator.py
@@ -11,25 +11,67 @@ class MazeGenerator:
         }
 
     def __init__(self, width: int, height: int, seed: int | None = None):
-        self.width = width
-        self.height = height
+        self.width_x = width
+        self.height_y = height
         self.seed = seed
 
         if seed is not None:
             random.seed(seed)
 
-        self.grid = self._create_grid(self.width, self.height)   
+        self.grid = self._create_grid(self.width_x, self.height_y)   
 
-    def _create_grid(self,width_x: int, height_y: int) -> list[list[Cell]]:
+    def _create_grid(self,) -> list[list[Cell]]:
         grid = []#erişim için grid[Y][X] önce satır sonra stun
         
-        for y in range(height_y):# DIŞ döngü: kaç satır olacağını belirler (y = satır numarası)
+        for y in range(self.height_y):# DIŞ döngü: kaç satır olacağını belirler (y = satır numarası)
             row = []# yeni satır için boş liste aç
             
-            for x in range(width_x): #her satırda width kadar eleman olustur
+            for x in range(self.width_x): #her satırda width kadar eleman olustur
                 cell = Cell()
                 row.append(cell)# ve o satıra ekle
                 
             grid.append(row)# sonra satırları grid de birlestir (satırlar listesi)
 
         return grid
+
+    def _generate_maze(self):
+        start_x, start_y = 0, 0
+        visited: set[tuple[int, int]] = set()
+        stack:   list[tuple[int, int]]
+        
+        visited.add((start_x, start_y))
+        stack = [(start_x, start_y)]
+        while stack:
+            x, y = stack[-1]
+            
+            neighbors = self._get_unvisited_neighbors(x, y, visited)
+            
+            if neighbors:
+                new_x, new_y, direction = random.choice(neighbors)
+                self._remove_wall(x, y, new_x, new_y, direction)
+                    
+
+
+
+    def _get_unvisited_neighbors(
+        self, x: int, y: int, visited: set[tuple[int, int]]
+        ) -> list[tuple[int, int, str]]:
+
+        neighbors: list[tuple[int, int, str]] = []
+        
+        for direction, (dx, dy) in self._DIRECTIONS.items():#tüm yönleri gez
+            new_x = x + dx
+            new_y = y + dy
+            
+            if not (0 <= new_x < self.width_x and 0 <= new_y < self.height_y):
+                continue
+            
+            if (new_x, new_y) in visited:
+                continue
+            
+            neighbors.append((new_x, new_y, direction))
+        
+        return neighbors
+
+    def _remove_wall(self, x: int, y: int, new_x: int, new_y, direction: str) -> None:
+        pass
