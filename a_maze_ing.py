fb9a32eaeea85c9713d44cff8866601e7b7582db
Muhammed Ömer CELEP <celepm82@gmail.com>
2026-08-19 13:01:27 +0000

    read_config update for read argv

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,63 +1,69 @@
 #!/usr/bin/env python3
+# from mazegen import MazeGenerator
+
+
+# def print_maze_ascii(gen: MazeGenerator) -> None:
+#     """Labirenti terminale basar - '42' desenindeki kilitli hücreleri '#' ile,
+#     normal koridorları boşluk ile gösterir.
+#     """
+#     width, height = gen.width_x, gen.height_y
+
+#     print("+" + "---+" * width)
+
+#     for y in range(height):
+#         # hücre içi + doğu duvarları
+#         line = "|"
+#         for x in range(width):
+#             cell = gen.grid[y][x]
+#             line += "###" if (x, y) in gen.locked_cells else "   "
+#             line += "|" if cell.east else " "
+#         print(line)
+
+#         # güney duvarları
+#         line = "+"
+#         for x in range(width):
+#             cell = gen.grid[y][x]
+#             line += "---" if cell.south else "   "
+#             line += "+"
+#         print(line)
+
+
+# if __name__ == "__main__":
+#     gen = MazeGenerator(width=15, height=9, perfect=True)
+#     print("=== PERFECT=True (15x9, seed=1) ===\n")
+#     print_maze_ascii(gen)
+
+#     print("\n\n=== PERFECT=False (15x9, seed=1) ===\n")
+#     gen2 = MazeGenerator(width=15, height=11, perfect=False)
+#     print_maze_ascii(gen2)
+
+
+#!/usr/bin/env python3
+from process_config import read_config_file
+from process_config import validate_config
 from mazegen import MazeGenerator
+import sys
 
 
-def print_maze_ascii(gen: MazeGenerator) -> None:
-    """Labirenti terminale basar - '42' desenindeki kilitli hücreleri '#' ile,
-    normal koridorları boşluk ile gösterir.
-    """
-    width, height = gen.width_x, gen.height_y
+def main() -> None:
+    if len(sys.argv) != 2:
+        print("[ERROR] Usage: python3 a_maze_ing.py <config_file>")
+        sys.exit(1)
 
-    print("+" + "---+" * width)
-
-    for y in range(height):
-        # hücre içi + doğu duvarları
-        line = "|"
-        for x in range(width):
-            cell = gen.grid[y][x]
-            line += "###" if (x, y) in gen.locked_cells else "   "
-            line += "|" if cell.east else " "
-        print(line)
-
-        # güney duvarları
-        line = "+"
-        for x in range(width):
-            cell = gen.grid[y][x]
-            line += "---" if cell.south else "   "
-            line += "+"
-        print(line)
-
-
-if __name__ == "__main__":
-    gen = MazeGenerator(width=15, height=9, perfect=True)
-    print("=== PERFECT=True (15x9, seed=1) ===\n")
-    print_maze_ascii(gen)
-
-    print("\n\n=== PERFECT=False (15x9, seed=1) ===\n")
-    gen2 = MazeGenerator(width=15, height=11, perfect=False)
-    print_maze_ascii(gen2)
-
-
-# # #!/usr/bin/env python3
-# # from process_config import read_config_file
-# # from process_config import validate_config
-# # from mazegen import MazeGenerator
-
-
-# # def main() -> None:
-# #     try:
-# #         config = read_config_file()
-# #         validate_config(config)
-# #     except ValueError as e:
-# #         print(f"[ERROR] - {e}")
-# #     maze = MazeGenerator(
-# #         config["WIDTH"],
-# #         config["HEIGHT"],
-# #         config["ENTRY"],
-# #         config["EXIT"],
-# #         config["PERFECT"],
-# #         config["SEED"]
-# #     )
+    try:
+        config = read_config_file(sys.argv[1])
+        validate_config(config)
+    except (ValueError, FileNotFoundError) as e:
+        print(f"[ERROR] - {e}")
+        sys.exit(1)
+    maze = MazeGenerator(
+        config["WIDTH"],
+        config["HEIGHT"],
+        config["ENTRY"],
+        config["EXIT"],
+        config["PERFECT"],
+        config["SEED"]
+    )
     
-# # if __name__ == "__main__":
-# #     main()
+if __name__ == "__main__":
+    main()
--- process_config/read_config.py
+++ process_config/read_config.py
@@ -1,62 +1,66 @@
-def read_config_file() -> dict[str, int | str | bool | tuple[int, int]]:
+def read_config_file(filename: str) -> dict[str, int | str | bool | tuple[int, int]]:
     config = {}
-    with open("config.txt", "r") as file:
-        for line in file:#satır satır tüm dosyayı geziyor
-            line = line.strip()# strip sağdan soldan boslukları özel karakterleri siliyor \n gibi
 
-            if not line or line.startswith("#"):#bos satır varsa o satırı atlıyor | stswith ise belli ztn yorum satırları icin
-                continue
+    try: 
+        with open(filename, "r") as file:
+            for line in file:#satır satır tüm dosyayı geziyor
+                line = line.strip()# strip sağdan soldan boslukları özel karakterleri siliyor \n gibi
 
-            if line.count("=") != 1:# 1 den fazla = var mı diye kontrol
-                raise ValueError(
-                    f"{line}: Invalid format use 'KEY=value'"
-                )
+                if not line or line.startswith("#"):#bos satır varsa o satırı atlıyor | stswith ise belli ztn yorum satırları icin
+                    continue
 
-            key, value = line.split("=")#= a göre splitliyip atıyor
-            key = key.strip()# bastan sondan bosluk ve özel karakterleri sil
-            value = value.strip()
-
-            if key in config:# 2 kere yazılmıs mı kontrolü
-                raise ValueError(f"Duplicate key: '{key}'")
-
-            if key in ("WIDTH", "HEIGHT"):
-                try:
-                    value = int(value)
-                except ValueError:
+                if line.count("=") != 1:# 1 den fazla = var mı diye kontrol
                     raise ValueError(
-                        f"{key} Must be integer"
+                        f"{line}: Invalid format use 'KEY=value'"
                     )
 
-            elif key in ("ENTRY", "EXIT"):
-               try:
-                    x, y = value.split(",")
-                    value = (int(x), int(y))
-               except ValueError:
-                   raise ValueError(
-                    f"{key} must be in the format x,y with integer coordinates"
-                   )
-                   
-            elif key == "SEED":
-                try:
-                    value = int(value)
-                except ValueError:
-                    raise ValueError(
-                        f"{key} Must be integer"
-                    )
+                key, value = line.split("=")#= a göre splitliyip atıyor
+                key = key.strip()# bastan sondan bosluk ve özel karakterleri sil
+                value = value.strip()
 
-            elif key == "PERFECT":
-                if value == "True":
-                    value = True
-                elif value == "False":
-                    value = False
+                if key in config:# 2 kere yazılmıs mı kontrolü
+                    raise ValueError(f"Duplicate key: '{key}'")
+
+                if key in ("WIDTH", "HEIGHT"):
+                    try:
+                        value = int(value)
+                    except ValueError:
+                        raise ValueError(
+                            f"{key} Must be integer"
+                        )
+
+                elif key in ("ENTRY", "EXIT"):
+                    try:
+                            x, y = value.split(",")
+                            value = (int(x), int(y))
+                    except ValueError:
+                        raise ValueError(
+                            f"{key} must be in the format x,y with integer coordinates"
+                        )
+                    
+                elif key == "SEED":
+                    try:
+                        value = int(value)
+                    except ValueError:
+                        raise ValueError(
+                            f"{key} Must be integer"
+                        )
+
+                elif key == "PERFECT":
+                    if value == "True":
+                        value = True
+                    elif value == "False":
+                        value = False
+                    else:
+                        raise ValueError("PERFECT must be True or False")
+
+                elif key == "OUTPUT_FILE":
+                    pass
+
                 else:
-                    raise ValueError("PERFECT must be True or False")
+                    raise ValueError(f"Unknown key: '{key}'")
 
-            elif key == "OUTPUT_FILE":
-                pass
-
-            else:
-                raise ValueError(f"Unknown key: '{key}'")
-
-            config[key] = value
+                config[key] = value
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Config file not found: '{filename}'")
     return config
