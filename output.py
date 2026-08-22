8687e16aa4ccff2c1ac59cac3fa5ea70f7790672
Muhammed Ömer CELEP <celepm82@gmail.com>
2026-08-22 16:33:40 +0000

    outpufile spaces

--- output.py
+++ output.py
@@ -7,6 +7,6 @@ def output_maze(maze: MazeGenerator, file_name: str, path: str) -> None:
     with open(file_name, "w") as f:
         f.write(maze_to_hex(maze))
         f.write("\n")
-        f.write(f"{maze.entry_pos[0]}, {maze.entry_pos[1]}\n")
-        f.write(f"{maze.exit_pos[0]}, {maze.exit_pos[1]}\n")
+        f.write(f"{maze.entry_pos[0]},{maze.entry_pos[1]}\n")
+        f.write(f"{maze.exit_pos[0]},{maze.exit_pos[1]}\n")
         f.write(f"{path}\n")
